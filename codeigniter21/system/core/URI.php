4e570996e54ab968a28922f7e5afa65913687c65
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2014-06-08 01:46:39 +0200

    Remove uri detection from uri on CodeIgniter

--- codeigniter21/system/core/URI.php
+++ codeigniter21/system/core/URI.php
@@ -86,13 +86,6 @@ class CI_URI {
 	{
 		if (strtoupper($this->config->item('uri_protocol')) == 'AUTO')
 		{
-			// Is the request coming from the command line?
-			if (php_sapi_name() == 'cli' or defined('STDIN'))
-			{
-				$this->_set_uri_string($this->_parse_cli_args());
-				return;
-			}
-
 			// Let's try the REQUEST_URI first, this will work in most situations
 			if ($uri = $this->_detect_uri())
 			{
